3e219611d73e8b2e4ffc43c8533c9d85d1c83032
AmruthKarnam <amruthkarnam@gmail.com>
2023-10-28 01:42:31 +0530

    Initial Front end

---
+++ src/Login.js
@@ -0,0 +1,42 @@
+// src/Login.js
+import React, { useState } from 'react';
+
+function Login({ setUserType }) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    // Perform authentication here (For simplicity, using hardcoded values)
+    if (username === 'admin' && password === 'password') {
+      setUserType('admin');
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h2>Login</h2>
+      <form onSubmit={handleLogin}>
+        <div className="form-group">
+          <label>Username:</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
